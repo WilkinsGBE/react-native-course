bb8851542d4802b6cb4c8df41f7b19754680a7c6
WilkinsGBE <saintilwilkins@gmail.com>
2025-02-12 18:30:56 -0500

    1h52

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
 import Search from "@/components/search";
+import { Card, FeaturedCard } from "@/components/Cards";
 
 const Home = () => {
   return (
@@ -11,16 +12,34 @@ const Home = () => {
       <View className="px-5">
         <View className="flex flex-row itmes.center justify-between mt-5">
           <View className="flex flex-row items-center">
-            <Image source ={images.avatar} className="size-12 rounded-full"/>
+            <Image source={images.avatar} className="size-12 rounded-full" />
             <View className="flex flex-col items-start mt-2 justify-center">
-              <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
-              <Text className="text-base font-rubik-medium text-black-300">Wilkins</Text>
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                Wilkins
+              </Text>
             </View>
           </View>
-          <Image source={icons.bell} className="size-6"/>
+          <Image source={icons.bell} className="size-6" />
         </View>
         <Search />
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
+      <FeaturedCard />
+      <Card />
     </SafeAreaView>
   );
 };
