5e42327bcf950f04d0442d41fb792145f7af7a8b
WilkinsGBE <saintilwilkins@gmail.com>
2025-02-13 17:45:30 -0500

    Finalized the homepage

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import images from "@/constants/images";
 import icons from "@/constants/icons";
 import Search from "@/components/search";
 import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
 
 const Home = () => {
   return (
@@ -36,7 +37,29 @@ const Home = () => {
               </Text>
             </TouchableOpacity>
           </View>
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
         </View>
+
+        <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Our Recommendation
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <Filters />
+          <View className="flex flex-row gap-5 mt-5">
+            <Card/>
+            <Card/>
+          </View>
+
       </View>
       <FeaturedCard />
       <Card />
