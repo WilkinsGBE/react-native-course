429c85f1e2cd5043789225334e25ee7403802f81
WilkinsGBE <saintilwilkins@gmail.com>
2025-02-18 09:33:26 -0500

    fetch the properties

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,91 +1,156 @@
-import { Client, Avatars, Account, OAuthProvider, Databases } from "react-native-appwrite";
-import * as Linking from 'expo-linking';
+import {
+  Client,
+  Avatars,
+  Account,
+  OAuthProvider,
+  Databases,
+  Query,
+} from "react-native-appwrite";
+import * as Linking from "expo-linking";
 import { openAuthSessionAsync } from "expo-web-browser";
 
 export const config = {
-    platform: 'com.gbe.restate',
-    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
-    galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
-    reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
-    agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
-    propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
-}
-  
+  platform: "com.gbe.restate",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  galleriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+  propertiesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
+};
+
 export const client = new Client();
 
 client
-    .setEndpoint(config.endpoint!)
-    .setProject(config.projectId!)
-    .setPlatform(config.platform!)
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform!);
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
 export const databases = new Databases(client);
 
 export async function login() {
-    try {
-        const redirectUrl = Linking.createURL('/');
-        console.log("Redirect URL:", redirectUrl); // Debugging
-        const response = await account.createOAuth2Token(OAuthProvider.Google, redirectUrl)
-    
-        console.log("OAuthProvider.Google:", OAuthProvider.Google);
+  try {
+    const redirectUrl = Linking.createURL("/");
+    console.log("Redirect URL:", redirectUrl); // Debugging
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUrl
+    );
 
-        if (!response) throw new Error("Failed to login");
+    console.log("OAuthProvider.Google:", OAuthProvider.Google);
 
-        const browserResult = await openAuthSessionAsync(
-            response.toString(),
-            redirectUrl
-        );
+    if (!response) throw new Error("Failed to login");
 
-        console.log("Browser Result:", browserResult); // Debugging
+    const browserResult = await openAuthSessionAsync(
+      response.toString(),
+      redirectUrl
+    );
 
-        if (browserResult.type != 'success') throw new Error("Failed to login.");
+    console.log("Browser Result:", browserResult); // Debugging
 
-        const url = new URL(browserResult.url);
+    if (browserResult.type != "success") throw new Error("Failed to login.");
 
-        const secret = url.searchParams.get('secret')?.toString();
-        const userId = url.searchParams.get('userId')?.toString();
+    const url = new URL(browserResult.url);
 
-        if (!secret || !userId) throw new Error("Failed to login.");
+    const secret = url.searchParams.get("secret")?.toString();
+    const userId = url.searchParams.get("userId")?.toString();
 
-        const session = await account.createSession(userId, secret);
+    if (!secret || !userId) throw new Error("Failed to login.");
 
-        if (!session) throw new Error("Failed to create a session");
+    const session = await account.createSession(userId, secret);
 
-        return true; //this means that we succesfully log in
+    if (!session) throw new Error("Failed to create a session");
 
-    } catch (error) {
-        console.error(error);
-        return false
-    }
+    return true; //this means that we succesfully log in
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
 
 export async function logout() {
-    try {
-        await account.deleteSession('current');
-        return true
-    } catch (error) {
-        console.error(error);
-        return false;
-    }
+  try {
+    await account.deleteSession("current");
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
 
 export async function getCurrentUser() {
-    try {
-        const response = await account.get();
-        if (response.$id) {
-            const userAvatar = avatar.getInitials(response.name);
+  try {
+    const response = await account.get();
+    if (response.$id) {
+      const userAvatar = avatar.getInitials(response.name);
 
-            return {
-                ...response,
-                avatar: userAvatar.toString(),
-            };
-        }
-        return response;
-    } catch (error) {
-        console.error(error);
-        return null;
+      return {
+        ...response,
+        avatar: userAvatar.toString(),
+      };
     }
+    return response;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function getLatestPropreties() {
+  try {
+    const result = await databases.listDocuments(
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      [Query.orderAsc("$createdAt"), Query.limit(50)]
+    );
+    return result.documents;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function getPropreties({
+  filter,
+  query,
+  limit,
+}: {
+  filter: string;
+  query: string;
+  limit?: number;
+}) {
+  try {
+    const buildQuery = [Query.orderDesc("$createdAt")];
+    if (filter && filter != "All") {
+      buildQuery.push(Query.equal("type", filter));
+    }
+
+    if (query) {
+      buildQuery.push(
+        Query.or([
+          Query.search("name", query),
+          Query.search("address", query),
+          Query.search("type", query),
+        ])
+      );
+    }
+    if (limit) {
+      buildQuery.push(Query.limit(limit));
+    }
+
+    const result = await databases.listDocuments(
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      buildQuery
+    );
+    return result.documents;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
